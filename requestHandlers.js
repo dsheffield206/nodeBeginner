1acd164721d0bee6563bff5ead27aedd1f4540ca
David Sheffield <david.sheffield21@gmail.com>
2015-09-06 18:12:28 -0700

    updating add ls -lah

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,16 @@
 'use strict';
 
+var exec = require('child_process').exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
+  var content = 'empty';
 
-  function sleep(milliSeconds){
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
+  exec('ls -lah', function(error, stdout, stderr){
+    content = stdout;
+  });
 
-  sleep(10000);
-  return 'Hello Start';
+  return content;
 }
 
 function upload() {
