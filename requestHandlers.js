82458d48da324e817734854cd7521f8afabd1d26
David Sheffield <david.sheffield21@gmail.com>
2015-09-06 19:38:00 -0700

    adding show handler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var querystring = require('querystring');
+var querystring = require('querystring'),
+    fs = require('fs');
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
@@ -31,5 +32,12 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
