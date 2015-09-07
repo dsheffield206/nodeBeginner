0f8cf799c8207f4ca66d16acae4d3bd134d0bbbf
David Sheffield <david.sheffield21@gmail.com>
2015-09-06 19:01:13 -0700

    adding input form with html elements

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,24 @@
 'use strict';
 
-var exec = require('child_process').exec;
-
 function start(response) {
   console.log("Request handler 'start' was called.");
-  // var content = 'empty';
 
-  exec('ls -lah', function(error, stdout, stderr) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(stdout);
+  var body = '<html>' +
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charste=UTF-8" />'+
+    '</head>'+
+    '</body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(body);
     response.end();
-  });
-  // return content;
 }
 
 function upload(response) {
