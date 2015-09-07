1c6db256c728c5a157d4b2056dfa0c556717d805
David Sheffield <david.sheffield21@gmail.com>
2015-09-06 19:17:52 -0700

    updating server.js for postDataChunk

--- server.js
+++ server.js
@@ -3,18 +3,24 @@
 var http = require('http');
 var url = require('url');
 
-
 function start(route, handle) {
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(handle, pathname, response);
+    request.setEncoding('utf8');
 
-    // response.writeHead(200, {'Content-Type': 'text/plain'});
-    // var content = route(handle, pathname);
-    // response.write(content);
-    // response.end();
+    request.addListner('data', function() {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '"+
+      postDataChunk + "'.");
+    });
+
+    request.addListner('end', function() {
+      route(handle, pathname, response, postData);
+    });
+
   }
 
   http.createServer(onRequest).listen(8888);
